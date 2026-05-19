e78f718ca6322b14800278162ded0a19d9182eae
Tyler Brazier <tyler@brazier.us>
2026-05-19 18:58:48 -0500

    Add initial gh.lua with :PR command
    Planning to rewrite this whole plugin in lua so the gh() function can be
    exposed and used by another plugin I'm planning to write where gx can be
    used on a commit hash to open that hash in github

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ Usage:
 Most commonly I use this to open the current file on the current line:
 
     :GH origin HEAD % .`
+
+To open a pull request from Neovim after pushing a branch:
+
+    :PR [title of pull request]
+
+this also populates the description using the commit messages on the branch.
---
+++ lua/gh.lua
@@ -0,0 +1,47 @@
+local function git(args)
+	local result = vim.system(vim.split('git '..args, ' ')):wait()
+	if result.code ~= 0 then error(result.stderr) end
+	return vim.trim(result.stdout)
+end
+
+local function master()
+	local result = git('rev-parse --abbrev-ref origin/HEAD')
+	-- e.g. origin/master (need to trim the origin/ part)
+	return result:match('origin/(.+)')
+end
+
+local function repo()
+	local remote_url = git('remote get-url origin')
+	-- e.g. git@github.com:tylerbrazier/vim-gh(.git)
+	return remote_url:match(':(.-)%.?g?i?t?$')
+end
+
+local function pr_description()
+	local m, n, f
+	m = master()
+	n = tonumber(git('rev-list --count --no-merges '..m..'..'))
+	f = n == 1 and '%b' or '-%x20%s%n%b'
+	return git('log --reverse --no-merges '..m..'.. --format='..f)
+end
+
+local function url_encode(str)
+	if not str then return '' end
+	return str:gsub(" ", "+"):gsub("([^%+%w%-_%.~])", function(c)
+		return string.format("%%%02X", string.byte(c))
+	end)
+end
+
+vim.api.nvim_create_user_command('PR',
+	function(opts)
+		local branch = git('branch --show-current')
+		local q = string.format('expand=1&title=%s&body=%s',
+			url_encode(opts.args),
+			url_encode(pr_description())
+		)
+		vim.ui.open(
+			string.format('https://github.com/%s/compare/%s?%s',
+				repo(), branch, q)
+		)
+	end,
+	{ nargs = '*' }
+)
